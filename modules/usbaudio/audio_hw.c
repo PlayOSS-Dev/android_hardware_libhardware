5184022affaea14a0edc90dabe3c752e6f02c922
pj <moonsin.ph@hotmail.com>
2012-08-16 15:18:33 +0700

    temp. fix usbudio error

--- modules/usbaudio/audio_hw.c
+++ modules/usbaudio/audio_hw.c
@@ -274,7 +274,7 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
     out->stream.set_volume = out_set_volume;
     out->stream.write = out_write;
     out->stream.get_render_position = out_get_render_position;
-    out->stream.get_next_write_timestamp = out_get_next_write_timestamp;
+    //out->stream.get_next_write_timestamp = out_get_next_write_timestamp;
 
     out->dev = adev;
 
